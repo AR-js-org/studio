303a6c96c022f3bc5ca99e5ad1a62f57789d1ad9
sigalor <nalenz@protonmail.com>
2019-12-24 23:04:11 +0100

    Change deployment script to clean directory in advance

--- src/components/common/Routes/Routes.js
+++ src/components/common/Routes/Routes.js
@@ -6,7 +6,6 @@ import Startpage from '../../pages/Startpage/Startpage.js';
 
 export default class Routes extends React.Component {
   render() {
-    console.log('PUBLIC_URL:', process.env.PUBLIC_URL);
     return (
       <ListeningBrowserRouter basename={process.env.PUBLIC_URL}>
         <Route exact path="/" component={Startpage} />
