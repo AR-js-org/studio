dff9fe4f96c69245f703ced5076f0fd5df78172c
sigalor <nalenz@protonmail.com>
2019-12-24 22:57:53 +0100

    Add process.env debug output

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import './assets/i18n/i18n.js';
 import Root from './components/common/Root/Root.js';
 
+console.log(JSON.stringify(process.env));
 ReactDOM.render(<Root />, document.getElementById('main'));
