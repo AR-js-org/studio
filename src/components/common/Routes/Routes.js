82e59bd6a84ad7b715544e4e80c46260a0fb0ab8
sigalor <nalenz@protonmail.com>
2019-12-25 12:09:27 +0100

    Bump Routes

--- src/components/common/Routes/Routes.js
+++ src/components/common/Routes/Routes.js
@@ -6,6 +6,7 @@ import Startpage from '../../pages/Startpage/Startpage.js';
 
 export default class Routes extends React.Component {
   render() {
+    console.log(JSON.stringify(process.env));
     return (
       <ListeningBrowserRouter basename={process.env.PUBLIC_URL}>
         <Route exact path="/" component={Startpage} />
