27b5105285af1cbfd6de419fadc1e47491f0f357
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-02-26 09:21:53 +0100

    code intentation

--- components/js/main.js
+++ components/js/main.js
@@ -2,20 +2,20 @@ let radioState = ""
 
 /* Update global radioState based on which Onclick is called from */
 function radioOnclick(self) {
-  radioState = self.getAttribute('id')
-  console.log(radioState)
+    radioState = self.getAttribute('id')
+    console.log(radioState)
 }
 
-/* 
+/*
 Redirects user based on the radioState.
 If the radioState is not selected, displays an error.
 */
 function startBuildingOnclick() {
-  if (radioState == "marker") {
-    window.location.href = BASE_URL + "/pages/marker.html"
-  } else if (radioState == "location") {
-    window.location.href = BASE_URL + "/pages/location.html"
-  } else {
-    // Display error message
-  }
+    if (radioState == "marker") {
+        window.location.href = BASE_URL + "/pages/marker.html"
+    } else if (radioState == "location") {
+        window.location.href = BASE_URL + "/pages/location.html"
+    } else {
+        // Display error message
+    }
 }
