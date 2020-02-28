a6ffe9ca1ea21d646c07fa4566cc4bb01d85256d
Mingi Ryu <mingir2@illinois.edu>
2020-02-27 18:18:34 -0600

    Change start button to an anchor element radio buttons now modify start button href directly

--- components/js/main.js
+++ components/js/main.js
@@ -1,20 +1,12 @@
-let radioState = ""
-
-/* Update global radioState based on which Onclick is called from */
+/* Update start building anchor href location based on radioState */
 function radioOnclick(self) {
-    radioState = self.getAttribute('id')
-    console.log(radioState)
-}
+    const radioState = self.getAttribute('id')
+    let startBuilding = document.getElementById("start-building")
 
-/*
-Redirects user based on the radioState.
-If the radioState is not selected, displays an error.
-*/
-function startBuildingOnclick() {
     if (radioState == "marker") {
-        window.location.href = BASE_URL + "/pages/marker.html"
+        startBuilding.href = BASE_URL + "/pages/marker.html"
     } else if (radioState == "location") {
-        window.location.href = BASE_URL + "/pages/location.html"
+        startBuilding.href = BASE_URL + "/pages/location.html"
     } else {
         // Display error message
     }
