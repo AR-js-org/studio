7ace454dbd7b47b1e7f3a72a3a2b9638b8902fda
faace <faace@qq.com>
2020-05-03 15:25:18 -0400

    let the 'file-holder' components clickable.

--- components/file-holder/file-holder.js
+++ components/file-holder/file-holder.js
@@ -28,16 +28,25 @@ const fileHolderTemplate = `
   </div>`;
 
 class FileHolder extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        var shadow = this.attachShadow({ mode: 'open' });
-        shadow.innerHTML = fileHolderTemplate;
-    }
+    var shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML = fileHolderTemplate;
+    setTimeout(function () {
+      var targetAttr = this.getAttribute('target');
+      var target = document.querySelector('#' + targetAttr)
+      if (target) {
+        this.onclick = function () {
+          target.click();
+        };
+      }
+    }.bind(this), 500);
+  }
 
-    connectedCallback() {
-        this.classList.add('pages-content-element');
-    }
+  connectedCallback() {
+    this.classList.add('pages-content-element');
+  }
 }
 
 customElements.define('file-holder', FileHolder);
