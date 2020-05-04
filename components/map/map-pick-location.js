3401c5246e9a8e20c3a5968b135c01c109cec132
ba5eem <baseem@gmail.com>
2020-05-03 16:06:28 -1000

    issue 48 func. mostly done, errors to address before PR

--- components/map/map-pick-location.js
+++ components/map/map-pick-location.js
@@ -1,8 +1,13 @@
 
 
+
+
+
 let map;
 let layerGroup;
 let shadow;
+let array = [];
+let markerId = [];
 
 // filter config array returning by uri path name for ex. location.html => {} config for this page
 // if you want to use the map on a different page for ex. differentpage.html => {} config for this page
@@ -24,10 +29,31 @@ function reUseMapComponent(path) {
     return MapTemplate;
 }
 
-function updateMarker(map, lat, lng, layerGroup) {
+
+function updateMyLocationMarker(lat, lng) {
+    array.push({
+                id: 'mylocation',
+                coords: [lat, lng],
+                lat: lat,
+                lng: lng
+            })
     map.panTo(new L.LatLng(lat, lng)); // will pan map to make the center of map the newly located coords
-    layerGroup.clearLayers(); // if updated again - clear previous markers from layergroup
-    L.marker([lat, lng]).addTo(layerGroup); // adds marker to the map
+    array.map(e => {
+        L.marker(e.coords).addTo(layerGroup);
+    }) 
+}
+
+function updateMarker() {
+    let key = "id";
+    const unique = [...new Map(array.map(item =>
+  [item[key], item])).values()];
+    layerGroup.clearLayers();
+
+    map.panTo(new L.LatLng(unique[0].lat, unique[0].lng)); // will pan map to make the center of map the newly located coords
+    unique.map(e => {
+
+        L.marker(e.coords).addTo(layerGroup);
+    }) 
 }
 
 function updateLatLngValue(lat, lng) {
@@ -35,20 +61,27 @@ function updateLatLngValue(lat, lng) {
     document.getElementById("longitude").value = lng.toString();
 }
 
-function check_lat_lon() {
+function check_lat_lon(e) {
+
     let regex_lat = /^(-?[1-8]?\d(?:\.\d{1,18})?|90(?:\.0{1,18})?)$/;
     let regex_lng = /^(-?(?:1[0-7]|[1-9])?\d(?:\.\d{1,18})?|180(?:\.0{1,18})?)$/;
-    let lat = document.getElementById(`latitude`).value;
-    let lng = document.getElementById(`longitude`).value;
+    let lat = document.getElementById(`latitude${e.target.name}`).value;
+    let lng = document.getElementById(`longitude${e.target.name}`).value;
+
     let validLat = regex_lat.test(lat); // 21.2908
     let validLng = regex_lng.test(lng); // -157.8305
 
     // only fire invalid coords if length on both is not 0
     if (lat.length !== 0 && lng.length !== 0) {
         if (validLat && validLng) {
-            updateMarker(map, lat, lng, layerGroup);
-            updateLatLngValue(lat, lng)
-            updateLatLngInnerHtml(lat, lng);
+            array.push({
+                id: e.target.name,
+                coords: [lat, lng],
+                lat: lat,
+                lng: lng
+            })
+            updateMarker();
+            updateLatLngInnerHtml();
         }
         else {
             updateLatLngInnerHtmlInvalidCoords()
@@ -56,14 +89,19 @@ function check_lat_lon() {
     }
 }
 
-function updateLatLngInnerHtml(lat, lng) {
-    lat = lat.toFixed(7);
-    lng = lng.toFixed(7);
+function updateLatLngInnerHtml() {
+
+
+    let allCoords = [];
+    array.map(e => {
+        allCoords.push(Number(e.lat).toFixed(7))
+        allCoords.push(Number(e.lng).toFixed(7))
+    })
 
     shadow.querySelector('.set-location-button').disabled = false;
     let x = shadow.querySelector(".location-set-display");
     x.style.visibility = "visible"
-    x.innerHTML = `Location is set to: <b>${lat.toString()}, ${lng.toString()}</b>`;
+    x.innerHTML = `Location is set to: <b>[${allCoords.toString()}]</b>`;
 }
 
 function updateLatLngInnerHtmlDenied(msg) {
@@ -82,11 +120,72 @@ function createButtonUseMyLocation() {
     let buttonUseMyLocation = document.createElement('button');
     buttonUseMyLocation.innerHTML = `${defineLocationSvg} Use my location`;
     buttonUseMyLocation.className = "use-my-location-button";
-    buttonUseMyLocation.style = `
-        background-color: white;
-        color: black;
-        margin-left: 2px`;
     return buttonUseMyLocation;
+}
+// let x = `<input class="text-input" type="text" id="latitude" onblur="check_lat_lon()" name="latitude" />`;
+function createInput(name, i) {
+    let coordInput = document.createElement('input');
+    coordInput.className = "text-input coordinates-inputs";
+    coordInput.type = 'text';
+    coordInput.id = `${name}${i}`;
+    coordInput.name = i;
+    coordInput.addEventListener('blur', check_lat_lon)
+    return coordInput;
+}
+
+
+function deleteCoords(e){
+    let id = e.target.id;
+    let lat = document.getElementById(`latitude${id}`);
+    lat.remove();
+    let lng = document.getElementById(`longitude${id}`);
+    lng.remove();
+    document.getElementById(id).remove();
+    e.target.remove();
+
+    if (lat.value.length !== 0 && lng.value.length !== 0) {
+        let arr = array.filter(e => e.id !== id);
+        array = arr;
+        console.log('fuck', array)
+        layerGroup.clearLayers();
+        updateMarker();
+    }
+    
+    
+}
+
+
+function updateLatLngInputs(i){
+    let numTags = document.getElementsByClassName('input-number-tags');
+    let latElems = document.getElementsByClassName('latitude-elements');
+    let lngElems = document.getElementsByClassName('longitude-elements');
+    let delCoords = document.getElementsByClassName('delete-coordinates-container');
+    let tagCount = document.getElementsByClassName('num-tags').length;
+    let count = tagCount + 1;
+    let numTag = document.createElement('p');
+    numTag.innerHTML = count + '.';
+    numTag.className = "num-tags";
+    numTag.id = count.toString();
+
+
+
+    let delIconDiv = document.createElement('div');
+    delIconDiv.className = 'delete-icon-parent';
+    delIconDiv.id = count.toString();
+
+    let delIcon = document.createElement('svg');
+    delIcon.innerHTML = trashCanSvg;
+    delIcon.className = 'hidden-delete-icon';
+
+
+    delIconDiv.addEventListener('click', deleteCoords)
+    
+
+    numTags[0].append(numTag);
+    latElems[0].append(createInput('latitude', count))
+    lngElems[0].append(createInput('longitude', count))
+    delCoords[0].append(delIconDiv)
+    delIconDiv.append(delIcon)
 }
 
 
@@ -101,11 +200,13 @@ class MapPickLocation extends HTMLElement {
         this.shadow = this.attachShadow({ mode: 'open' });
         this.shadow.innerHTML = reUseMapComponent(this.path);
         this.mapRoot = this.shadow.querySelector(this.mapConfig.className);
+
     }
 
     connectedCallback() {
         shadow = this.shadow;
         this.createMap();
+        this.addLatLngInputs();
 
         let setLocationButton = this.shadowRoot.querySelector('.set-location-button');
         setLocationButton.addEventListener("click", (e) => {
@@ -113,6 +214,14 @@ class MapPickLocation extends HTMLElement {
             let lng = document.getElementById(`longitude`).value;
             alert(`progress to next screen, ${lat}, ${lng}`);
         })
+
+        let addIcon = document.getElementsByClassName('add-location-icon');
+        addIcon[0].addEventListener('click', function(){
+            updateLatLngInputs(1)
+
+        })
+
+
 
     }
 
@@ -122,8 +231,13 @@ class MapPickLocation extends HTMLElement {
         L.tileLayer(tile_url, this.mapConfig.attribution_opts).addTo(map);
         map.on('click', function(e) { // => {} that contains the coordinates
 
-            updateMarker(map, e.latlng.lat, e.latlng.lng, layerGroup);
-            updateLatLngValue(e.latlng.lat, e.latlng.lng);
+            array.push({
+                id: 'mapclick',
+                coords: [e.latlng.lat, e.latlng.lng],
+                lat: e.latlng.lat,
+                lng: e.latlng.lng
+            })
+            updateMarker();
             updateLatLngInnerHtml(e.latlng.lat, e.latlng.lng)
         })
 
@@ -135,8 +249,8 @@ class MapPickLocation extends HTMLElement {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function(position) {
                     updateLatLngInnerHtml("Locating", "....");
-                    updateMarker(map, position.coords.latitude, position.coords.longitude, layerGroup);
-                    updateLatLngValue(position.coords.latitude, position.coords.longitude);
+                    
+                    updateMyLocationMarker(position.coords.latitude, position.coords.longitude);
                     updateLatLngInnerHtml(position.coords.latitude, position.coords.longitude)
                 },
                     function(error) {
@@ -153,6 +267,85 @@ class MapPickLocation extends HTMLElement {
             }
         })
     }
+
+
+
+
+    addLatLngInputs(){
+
+        const parent = document.getElementsByClassName('coordinates-input-wrapper')[0];
+        const coordsParent = document.createElement('div');
+        coordsParent.className = 'coordinates-input-container';
+        parent.append(coordsParent);
+
+        const numTags = document.createElement('div');
+        numTags.className = 'input-number-tags';
+
+        const latElems = document.createElement('div');
+        latElems.className = 'latitude-elements';
+
+        const lngElems = document.createElement('div');
+        lngElems.className = 'longitude-elements';
+
+        const latElemLabel = document.createElement('label');
+        latElemLabel.for = 'latitude';
+        latElemLabel.innerHTML = 'Latitude:';
+
+        const lngElemLabel = document.createElement('label');
+        lngElemLabel.for = 'longitude';
+        lngElemLabel.innerHTML = 'Longitude:'
+
+        const delCoords = document.createElement('div');
+        delCoords.className = 'delete-coordinates-container';
+
+        const addCoords = document.createElement('div');
+        addCoords.className = 'add-location-container';
+        const addCoordsIcon = document.createElement('div');
+        addCoordsIcon.className = 'add-location-icon';
+        addCoordsIcon.innerHTML = plusSignSvg;
+
+        const addCoordsName = document.createElement('div');
+        addCoordsName.className = 'add-location-name';
+        addCoordsName.innerHTML = 'add location';
+
+
+        latElems.append(latElemLabel); // static
+        lngElems.append(lngElemLabel); // static
+        coordsParent.append(numTags); // static
+        coordsParent.append(latElems); // static
+        coordsParent.append(lngElems); // static
+        coordsParent.append(delCoords); // static
+        parent.append(addCoords); // static
+        addCoords.append(addCoordsIcon); // static
+        addCoords.append(addCoordsName); // static
+
+
+        let numTag = document.createElement('p');
+        numTag.innerHTML = 1 + '.';
+        numTag.className = "num-tags";
+        let delIconDiv = document.createElement('div');
+        delIconDiv.className = 'delete-icon-parent';
+        delIconDiv.id = "1";
+
+        let delIcon = document.createElement('svg');
+        delIcon.innerHTML = trashCanSvg;
+        delIcon.className = 'hidden-delete-icon';
+        delIcon.style.visibility = 'hidden';
+
+        delIconDiv.addEventListener('click', function(e){
+            let item = e.target.id;
+
+        })
+        
+        numTags.append(numTag);
+        latElems.append(createInput('latitude', 1))
+        lngElems.append(createInput('longitude', 1))
+        delCoords.append(delIconDiv)
+        delIconDiv.append(delIcon)
+    }
+
+
+    
 
 
 
