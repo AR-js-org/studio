dc12c1da8f70d23f13009dfef2f88b7f911931a9
mingir2 <mingir2@illinois.edu>
2020-05-19 18:29:18 -0500

    Implement publish function Uses session storge to store all the uploaded data.

--- pages/marker.js
+++ pages/marker.js
@@ -1,6 +1,13 @@
 const { MarkerModule, Package } = ARjsStudioBackend;
 
-const zip = () => {
+/**
+ * Packages the data into a zip file and prompts download.
+ * 
+ * TODO: Add the generated markerImage into the zip package.
+ * 
+ * @param {event} event 
+ */
+const zip = (event) => {
     // TODO: replace alerts with HTML error messages.
     if (!window.markerImage) return alert('please select a marker image');
     if (!window.assetType) return alert('please select the corret content type');
@@ -25,10 +32,38 @@ const zip = () => {
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
-
-
         });
+    event.preventDefault();
 };
 
+/**
+ * Stores the session data and redirects to publish page.
+ * 
+ * @param {event} event 
+ */
+const publish = (event) => {
+    // TODO: replace alerts with HTML error messages.
+    if (!window.markerImage) return alert('please select a marker image');
+    if (!window.assetType) return alert('please select the corret content type');
+    if (!window.assetFile || !window.assetName) return alert('please upload a content');
+
+    MarkerModule.getMarkerPattern(window.markerImage)
+    .then((markerPattern) => {
+        const session = {
+            arType: 'pattern',
+            assetType: window.assetType, // image/audio/video/3d
+            assetFile: window.assetFile,
+            assetName: window.assetName,
+            assetParam: window.assetParam,
+            markerPatt: markerPattern,
+            markerImage: window.markerImage,
+        }
+        sessionStorage.setItem("session", JSON.stringify(session));
+        window.location = "./publish";
+    })
+    event.preventDefault();
+}
+
 const element = document.querySelector("page-footer");
-element.addEventListener("onClick", zip);
+element.addEventListener("zip-button", zip);
+element.addEventListener("publish-button", publish);
