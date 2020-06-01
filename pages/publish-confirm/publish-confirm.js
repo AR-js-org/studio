633022f484cda5682d3c6ef9a39c19b5fb3425d8
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-06-01 21:14:03 +0200

    fix wrong call

--- pages/publish-confirm/publish-confirm.js
+++ pages/publish-confirm/publish-confirm.js
@@ -18,19 +18,20 @@ window.onload = async () => {
     let response = await fetch(`https://arjs-studio-backend.herokuapp.com/authenticate/${queryDict.code}`);
     response = await response.json();
 
-    // let's publish on user's repo
-    const githubResponse = await github.serveFiles({
-        token: response.token, // required, must be an OAuth2 token
-        message: 'first commit for WebAR!', // optional
-        repo: window.session.projectName + window.session.randomString, // using user + GH code, gg wp
-        branch: 'gh-pages' // automatically deploy to Pages by default
-    });
+    const pagesUrl = await new Package(window.session)
+        .then((package) => package.serve({
+            packageType: 'github',
+            token: response.token, // required, must be an OAuth2 token
+            message: 'first commit for WebAR!', // optional
+            repo: window.session.projectName + window.session.randomString, // using user + GH code, gg wp
+            branch: 'gh-pages' // automatically deploy to Pages by default
+    }));
 
-    console.log(githubResponse);
+    console.debug(pagesUrl);
 
-    // document.querySelector('#input-container').remove();
-    // document.querySelector('.repo-url').innerHTML = `
-    //     We're done! Your Web AR Experience can be found at <p class="url">${githubResponse.url}</p>
-    //     Share it to make others enjoy your work!
-    // `;
+    document.querySelector('#input-container').remove();
+    document.querySelector('.repo-url').innerHTML = `
+        We're done! Your Web AR Experience can be found at <p class="url">${pagesUrl}</p>
+        Share it to make others enjoy your work!
+    `;
 };
