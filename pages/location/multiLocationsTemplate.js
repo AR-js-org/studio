e4e8d2e1f39e408ecf4d31e7a0744b37ed923d09
faace <faace@qq.com>
2020-06-14 13:59:39 -0400

    download  and upload multi locations

---
+++ pages/location/multiLocationsTemplate.js
@@ -0,0 +1,26 @@
+window.multiLocationsTemplate = `{
+    "showDistance": true, // true/false, if true, show distance from origin (distanceMsg property) for every place
+    "showName": true, // true/false, if true, show place name for every place
+    "heightFromGround": 0,  // if true, change Y position for every place
+    "places": [
+        {
+            "id": 0,
+            "name": "Colosseum",
+            "latitude": 0.0000,
+            "longitude": 0.000
+        },
+        {
+            "id": 1,
+            "name": "Arco di Traiano",
+            "latitude": 0.0000,
+            "longitude": 0.000
+        },
+        {
+            "id": 2,
+            "name": "Stadio Olimpico",
+            "latitude": 0.0000,
+            "longitude": 0.000
+        }
+        // ....
+    ]
+}`;
