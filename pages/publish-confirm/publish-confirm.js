6e336f7c566f95468dfe84825fdf5f52f43cdbac
Nicolò Carpignoli <nicolocarpignoli@gmail.com>
2024-03-18 10:51:59 +0100

    chore: use new oauth server

--- pages/publish-confirm/publish-confirm.js
+++ pages/publish-confirm/publish-confirm.js
@@ -18,7 +18,7 @@ window.onload = async () => {
         return;
     }
 
-    let response = await fetch(`https://arjs-studio-backend.herokuapp.com/authenticate/${queryDict.code}`);
+    let response = await fetch(`https://gatekeeper-arjsstudio.fly.dev/authenticate/${queryDict.code}`);
     response = await response.json();
 
     const package = new Package(window.session);
